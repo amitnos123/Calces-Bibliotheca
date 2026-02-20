9ea9eb9848c0c776e077402ff35836e4e411359f
sami.nosatzki <sami.nosatzki@gmail.com>
2026-02-20 05:04:36 +0200

    Made base modules files

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+const BASE_URL: &'static str = "https://stoat.chat";
+const API_ENDPOINT: &'static str = "/api";
+const EVENTS_ENDPOINT: &'static str = "/events";
+const FILES_ENDPOINT: &'static str = "https://cdn.stoatusercontent.com";
+const PROXY_ENDPOINT: &'static str = "https://external.stoatusercontent.com";
+
+mod users;
+mod bots;
+mod channels;
+mod servers;
+mod invites;
+mod customisation;
+mod administration;
+mod misc;
