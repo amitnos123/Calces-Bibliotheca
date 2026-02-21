fbd2fc73e51ba473cb6e5641e2e7fbaef59c0a2a
sami.nosatzki <sami.nosatzki@gmail.com>
2026-02-21 02:16:14 +0200

    made display name data model with tests

--- src/data_model/display_name.rs
+++ src/data_model/display_name.rs
@@ -10,7 +10,7 @@ impl DisplayName {
         if len < 2 || len > 32 {
             Err(format!("DisplayName String length must be 2..=32, got {}", len))
         } else {
-            let re = Regex::new(r"^[^\u{200B}\n\r]+$").unwrap();
+            let re = Regex::new(r"^[^\u200B\n\r]+$").unwrap();
             if !re.is_match(s) {
                 Err("DisplayName String most follow ^[^\\u{200B}\\n\\r]+$".to_string())
             } else {
@@ -43,4 +43,57 @@ impl<'de> Deserialize<'de> for DisplayName {
         let s = String::deserialize(deserializer)?;
         DisplayName::new(&s).map_err(serde::de::Error::custom)
     }
+}
+
+
+#[test]
+fn  test_display_name_regex_fail_1(){
+    let r = DisplayName::new("test\nlength\nvalidation\nfail");
+    assert_eq!(true, r.is_err());
+}
+#[test]
+fn  test_display_name_regex_fail_2(){
+    let r = DisplayName::new("test\rlength\rvalidation\rfail");
+    assert_eq!(true, r.is_err());
+}
+#[test]
+fn  test_display_name_regex_fail_3(){
+    let r = DisplayName::new("test\u{200B}length\u{200B}validation\u{200B}fail");
+    assert_eq!(true, r.is_err());
+}
+
+#[test]
+fn  test_display_name_length_regex_validation_success(){
+    let r = DisplayName::new("test length validation success");
+    assert_eq!(true, r.is_ok());
+}
+
+#[test]
+fn  test_display_name_max_length_validation_fail(){
+    let r = DisplayName::new(&"test length validation fail".repeat(3));
+    assert_eq!(true, r.is_err());
+}
+#[test]
+fn  test_display_name_min_length_validation_fail(){
+    let r = DisplayName::new(&"A");
+    assert_eq!(true, r.is_err());
+}
+
+#[test]
+fn test_display_name_serialization() {
+    let display_name = DisplayName("serialization".to_string());
+
+    let json = serde_json::to_string(&display_name).unwrap();
+
+    assert_eq!(json, r#""serialization""#);
+}
+
+#[test]
+fn test_display_name_deserialization() {
+    let json = r#""deserialization""#;
+
+    let display_name = serde_json::from_str::<DisplayName>(json).unwrap();
+
+    assert_eq!(display_name, DisplayName("deserialization".to_string()));
+    
 }
