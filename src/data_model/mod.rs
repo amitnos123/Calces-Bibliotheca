dfc9857d5f4704920a5b40ca6550167091f8a3d3
sami.nosatzki <sami.nosatzki@gmail.com>
2026-02-21 17:11:30 +0200

    made flags with bitflag crate

--- src/data_model/mod.rs
+++ src/data_model/mod.rs
@@ -5,7 +5,44 @@ pub mod display_name;
 pub mod data_user_profile;
 
 pub type Badges = i32;
-pub type Flags = i32;
+// pub type Flags = i32;
+
+#[bitflag::bitflag(i64)]
+#[derive(Clone, Copy, Serialize)]
+pub enum Flags {
+    ManageChannel = 1 << 0,
+    ManageServer = 1 << 1,
+    ManagePermissions = 1 << 2,
+    ManageRole = 1 << 3,
+    ManageCustomisation = 1 << 4,
+
+    KickMembers = 1 << 6,
+    BanMembers = 1 << 7,
+    TimeoutMembers = 1 << 8,
+    AssignRoles = 1 << 9,
+    ChangeNickname = 1 << 10,
+    ManageNicknames = 1 << 11,
+    ChangeAvatar = 1 << 12,
+    RemoveAvatars = 1 << 13,
+
+    ViewChannel = 1 << 20,
+    ReadMessageHistory = 1 << 21,
+    SendMessage = 1 << 22,
+    ManageMessages = 1 << 23,
+    ManageWebhooks = 1 << 24,
+    InviteOthers = 1 << 25,
+    SendEmbeds = 1 << 26,
+    UploadFiles = 1 << 27,
+    Masquerade = 1 << 28,
+    React = 1 << 29,
+    Connect = 1 << 30,
+    Speak = 1 << 31,
+    Video = 1 << 32,
+    MuteMembers = 1 << 33,
+    DeafenMembers = 1 << 34,
+    MoveMembers = 1 << 35,
+}
+
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub enum FieldsUser {
