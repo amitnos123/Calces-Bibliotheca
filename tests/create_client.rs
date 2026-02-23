d0b340d3b796c3584ae8afdff5b69861b36d9504
sami.nosatzki <sami.nosatzki@gmail.com>
2026-02-23 19:49:35 +0200

    added test if has token or not

--- tests/create_client.rs
+++ tests/create_client.rs
@@ -3,6 +3,9 @@ use calces_bibliotheca;
 #[tokio::test]
 async fn test_create_client() {
     let token = std::env::var("token").unwrap_or_else(|_| "token".to_string());
+    if token == "token" {
+        panic!("Please set env var 'token'");
+    }
 
     let result = calces_bibliotheca::create_client(token).await;
 
