f368d5e1f054d8e5c0a3fe42eacb11b46ac001d2
sami.nosatzki <sami.nosatzki@gmail.com>
2026-02-23 22:15:48 +0200

    comment tests that aren't ready

--- tests/tests/customisation/emojis.rs
+++ tests/tests/customisation/emojis.rs
@@ -1,39 +1,39 @@
-//TODO: Test
-#[tokio::test]
-async fn test_create_new_emoji() {
-    let token = std::env::var("token").unwrap_or_else(|_| "token".to_string());
+// //TODO: Test
+// #[tokio::test]
+// async fn test_create_new_emoji() {
+//     let token = std::env::var("token").unwrap_or_else(|_| "token".to_string());
 
-    let client = calces_bibliotheca::create_client(token).await;
+//     let client = calces_bibliotheca::create_client(token).await;
 
-    let result = client.unwrap().create_new_emoji("target", "name".to_string(), calces_bibliotheca::api::customisation::emojis::Parent::Detached, true).await;
+//     let result = client.unwrap().create_new_emoji("target", "name".to_string(), calces_bibliotheca::api::customisation::emojis::Parent::Detached, true).await;
 
-    println!("{:?}", result);
+//     println!("{:?}", result);
 
-    assert!(result.is_ok());
-}
-//TODO: Test
-#[tokio::test]
-async fn test_fetch_emoji() {
-    let token = std::env::var("token").unwrap_or_else(|_| "token".to_string());
+//     assert!(result.is_ok());
+// }
+// //TODO: Test
+// #[tokio::test]
+// async fn test_fetch_emoji() {
+//     let token = std::env::var("token").unwrap_or_else(|_| "token".to_string());
 
-    let client = calces_bibliotheca::create_client(token).await;
+//     let client = calces_bibliotheca::create_client(token).await;
 
-    let result = client.unwrap().fetch_emoji("target").await;
+//     let result = client.unwrap().fetch_emoji("target").await;
 
-    println!("{:?}", result);
+//     println!("{:?}", result);
 
-    assert!(result.is_ok());
-}
-//TODO: Test
-#[tokio::test]
-async fn test_delete_emoji() {
-    let token = std::env::var("token").unwrap_or_else(|_| "token".to_string());
+//     assert!(result.is_ok());
+// }
+// //TODO: Test
+// #[tokio::test]
+// async fn test_delete_emoji() {
+//     let token = std::env::var("token").unwrap_or_else(|_| "token".to_string());
 
-    let client = calces_bibliotheca::create_client(token).await;
+//     let client = calces_bibliotheca::create_client(token).await;
 
-    let result = client.unwrap().delete_emoji("target").await;
+//     let result = client.unwrap().delete_emoji("target").await;
 
-    println!("{:?}", result);
+//     println!("{:?}", result);
 
-    assert!(result.is_ok());
-}
+//     assert!(result.is_ok());
+// }
